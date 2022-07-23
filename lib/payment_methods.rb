e8b917e39b8aaf9f56b112de14f276196c70036a
dsnraghavendrarao <raghavann13@gmail.com>
2022-07-23 17:53:59 +0530

    Initial commit

--- lib/payment_methods.rb
+++ lib/payment_methods.rb
@@ -5,4 +5,11 @@ require_relative "payment_methods/version"
 module PaymentMethods
   class Error < StandardError; end
   # Your code goes here...
+  class Start
+    def self.modes
+      puts "Transaction Purchase"
+      puts "Bulk Purchase"
+      puts "Subscription Purchase"
+    end
+  end
 end
